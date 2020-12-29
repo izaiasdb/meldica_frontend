55e76684b698ebc22b58a60b2064fc2edc8d6d22
izaiasdb <izaias.barreto@gmail.com>
2020-12-29 11:18:54 -0300

    heroku port

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('*', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 
 // https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 // https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
-// const PORT = process.env.PORT || 3000 // Antes heroku
-const PORT = '0.0.0.0' || 3000
+const PORT = process.env.PORT || 3000 // Antes heroku
+//const PORT = '0.0.0.0' || 3000
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
     console.log('Press Ctrl+C to quit.')
