e040904c23dc48247e0e8b2c0227151327d666c5
izaiasdb <izaias.barreto@gmail.com>
2021-07-04 20:46:00 -0300

    tag status

--- src/pages/util/constUrlUtils.js
+++ src/pages/util/constUrlUtils.js
@@ -8,5 +8,5 @@
 // export const URL_FRONTEND = "http://localhost:8089/";
 
 //Produção
-export const URL_BACKEND = process.env.NODE_ENV === 'production' ? "https://meldica-backend.herokuapp.com" : "http://localhost:8081/meldica_backend";
+export const URL_BACKEND = process.env.NODE_ENV === 'production' ? "https://meldica-backend.herokuapp.com" : "http://localhost:8080/meldica_backend";
 export const URL_FRONTEND = process.env.NODE_ENV === 'production' ? "https://meldica-frontend.herokuapp.com/" : "http://localhost:8089/";
